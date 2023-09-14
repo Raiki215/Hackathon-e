002149f96474ce3df3de056d74b4137c87edf284
renka <r.oomori.sys22@morijyobi.ac.jp>
2023-09-14 11:50:32 +0900

    ログイン、新規登録画面のデザイン #37

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for,session, Bluepr
 from datetime import timedelta
 from user.user import user_bp
 from admin.admin import admin_bp
-<<<<<<< HEAD
 from task.task import task_bp
-=======
->>>>>>> 30c8a310f0f6dd81b5f0cb79d1f5f5388827426e
-from team.team import team_bp
 import string, random
 
 
@@ -17,8 +13,6 @@ app.secret_key = ''.join(random.choices(string.ascii_letters,k=256))
 app.register_blueprint(user_bp)
 app.register_blueprint(task_bp)
 app.register_blueprint(admin_bp)
-app.register_blueprint(team_bp)
-<<<<<<< HEAD
 app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route('/', methods=['GET'])
@@ -39,9 +33,6 @@ def mypage():
         return render_template('mypage.html')
     else :
         return redirect(url_for('index'))
-=======
-app.permanent_session_lifetime = timedelta(minutes=5)
->>>>>>> 30c8a310f0f6dd81b5f0cb79d1f5f5388827426e
 
 @app.route('/notification',methods=['GET'])
 def notification():
